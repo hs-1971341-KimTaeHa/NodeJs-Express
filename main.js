2af213867ba2082c1a33661d49516446707d3721
unknown <xogk1128@naver.com>
2022-07-25 17:39:28 +0900

    페이지 생성 구현

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+var qs = require('querystring');
 
 app.get('/', function(request, response) {
   fs.readdir('./data', function(error, filelist){
@@ -42,6 +43,42 @@ app.get('/page/:pageId', function(request, response) {
   });
 });
 
+app.get('/create', function(request, response) {
+  fs.readdir('./data', function(error, filelist){
+    var title = 'WEB - create';
+    var list = template.list(filelist);
+    var html = template.HTML(title, list, `
+      <form action="/create_process" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+          <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+    `, '');
+    response.writeHead(200);
+    response.end(html);
+  });
+});
+
+app.post('/create_process', function(request, response){
+  var body = '';
+  request.on('data', function(data){
+      body = body + data;
+  });
+  request.on('end', function(){
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+        response.writeHead(302, {Location: `/:pageId`});
+        response.end();
+      })
+  });
+})
+
 app.listen(3000, function() {
   console.log(`Example app listening on port 3000`);
 })
@@ -59,18 +96,7 @@ app.listen(3000, function() {
 //     var queryData = url.parse(_url, true).query;
 //     var pathname = url.parse(_url, true).pathname;
 //     if(pathname === '/'){
-//       if(queryData.id === undefined){
-//         fs.readdir('./data', function(error, filelist){
-//           var title = 'Welcome';
-//           var description = 'Hello, Node.js';
-//           var list = template.list(filelist);
-//           var html = template.HTML(title, list,
-//             `<h2>${title}</h2>${description}`,
-//             `<a href="/create">create</a>`
-//           );
-//           response.writeHead(200);
-//           response.end(html);
-//         });
+//       
 //       } else {
 //         
 //       }
